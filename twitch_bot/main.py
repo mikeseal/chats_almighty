47561bcc5ffaf402a1e284f0bdcf9530ade306e4
mikeseal <mike@sealpoint.net>
2024-09-25 17:30:14 +0100

    temp commit to add teh MVC initial at the app level.

--- twitch_bot/main.py
+++ twitch_bot/main.py
@@ -2,6 +2,7 @@ from os import getenv
 from dotenv import load_dotenv
 from twitch_bot.controller.chat_bot_controller import ChatBotController
 from pathlib import Path
+from model.models import Channels, Commands, Links
 
 # INFO this is the starting point of our program, it creates the controller which glues the model+view components together
 if __name__ == '__main__':
@@ -11,3 +12,21 @@ if __name__ == '__main__':
                                  "shepsalmighty")
 
     chat_bot.run()
+
+
+
+
+
+    model_channels = Channels()
+    model_commands = Commands())
+    model_links = Links()
+    
+    
+
+    view = UserView()
+    controller = UserController(model, view)
+    controller.add_user("Alice")
+    controller.add_user("Bob")
+    controller.show_users()
+if __name__ == "__main__":
+    main()
